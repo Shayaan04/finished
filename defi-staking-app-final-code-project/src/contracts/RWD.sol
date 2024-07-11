473170bea844bff864551cd542bc16954dfdf67a
Shayaan Mohammed <shayaanm2005@gmail.com>
2024-07-10 23:57:18 -0500

    changed the license

--- defi-staking-app-final-code-project/src/contracts/RWD.sol
+++ defi-staking-app-final-code-project/src/contracts/RWD.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.5.0;
-
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity ^0.8.19;
 contract RWD {
     string  public name = "Reward Token";
     string  public symbol = "RWD";
